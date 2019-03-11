b7b3ce70653213c76f0161211ea2275a5bcee954
martulioruiz <martulioruiz@gmail.com>
2019-03-11 17:09:05 -0600

    se creo interfaz de terminal y el control de comandos

--- Proyecto/Arbol Infinito JS/core/js/clases/dtas/Arbol.js
+++ Proyecto/Arbol Infinito JS/core/js/clases/dtas/Arbol.js
@@ -3,7 +3,7 @@ import Nodo from "./Nodo.js";
 export default class Arbol{
 
 	constructor(){
-		alert("soy un arbol")
+		console.log("soy un arbol")
 		this.raiz = null;
 	}
 
@@ -25,6 +25,65 @@ export default class Arbol{
 		}else{
 			nodoActual.setHijo(new Nodo(identificador,nodoActual));
 		}
-	}	
+	}
+
+	/*
+	------------------------------------------------------------
+	Este seccion que sigue es dedicada a los comandos
+	-----------------------------------------------------
+	*/	
+
+	comandoControl(comando){
+		var lista = comando.split(" ");
+		var parametros = [];
+		var palabraReservada = null;
+		var nombre = null;
+
+		for (var i = 0; i < lista.length; i++) {
+			if(i == 0){
+				palabraReservada = lista[i];
+			}else{
+				parametros.push(lista[i]);
+			}
+		}
+
+		
+		try{
+			nombre = "comando_"+palabraReservada;
+			this["comando_"+palabraReservada](parametros);
+			document.getElementById("comando").value = null;
+			document.getElementById("terminalMensaje").innerHTML = null;
+		}catch(err){
+			document.getElementById("terminalMensaje").innerHTML = "Error: comando no existe";
+			document.getElementById("comando").value = null;
+		}
+
+		
+		
+	}
+
+	comando_cd(parametros){
+		console.log("usted ah seleccionado el comando cd");
+	}
+
+	comando_mkdir(parametros){
+		console.log("usted ah seleccionado el comando mkdir");
+	}
+
+	comando_touch(parametros){
+		console.log("usted ah seleccionado el comando touch");
+	}
+
+	comando_find(parametros){
+		console.log("usted ah seleccionado el comando find");
+	}
+
+	comando_pwd(parametros){
+		console.log("usted ah seleccionado el comando pwd");
+	}
+
+	comando_ls(parametros){
+		console.log("usted ah seleccionado el comando ls");
+	}
 
 }
