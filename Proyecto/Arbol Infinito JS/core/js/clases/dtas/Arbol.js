929332dd00370c8a930fab09038aa0d6ab8725d1
martulioruiz <martulioruiz@gmail.com>
2019-03-11 18:56:41 -0600

    Se migraron los comandos mkdir y cd a arbol infinito js

--- Proyecto/Arbol Infinito JS/core/js/clases/dtas/Arbol.js
+++ Proyecto/Arbol Infinito JS/core/js/clases/dtas/Arbol.js
@@ -20,7 +20,8 @@ export default class Arbol{
 	}
 
 	agregarNodo(nodoActual, identificador){
-		if(nodoActual.getHijo(identificador) != null){
+		var nodo = nodoActual.getHijo(identificador);
+		if(nodo != null){
 			alert("error el nodo ya existe");
 		}else{
 			nodoActual.setHijo(new Nodo(identificador,nodoActual));
@@ -52,9 +53,9 @@ export default class Arbol{
 			nombre = "comando_"+palabraReservada;
 			this["comando_"+palabraReservada](parametros);
 			document.getElementById("comando").value = null;
-			document.getElementById("terminalMensaje").innerHTML = null;
+			document.getElementById("terminalError").innerHTML = null;
 		}catch(err){
-			document.getElementById("terminalMensaje").innerHTML = "Error: comando no existe";
+			document.getElementById("terminalError").innerHTML = err.message;
 			document.getElementById("comando").value = null;
 		}
 
@@ -62,27 +63,54 @@ export default class Arbol{
 		
 	}
 
-	comando_cd(parametros){
+	comando_cd(parametro){
 		console.log("usted ah seleccionado el comando cd");
+		var tipoParametro = parametro[0].split("/");
+		if(tipoParametro.length > 1){
+			console.log("ruta relativa");
+		}else{
+			if(tipoParametro[0] == ".."){
+				console.log("ruta retroceso");
+				if(this.raiz.padre == null){
+					document.getElementById("terminalMensaje").innerHTML = "Error: Se encuentra en la ruta raiz, no puede retroceder mas";
+				}else{
+					this.raiz = this.raiz.padre;
+					document.getElementById("terminalMensaje").innerHTML = "Ahora se encuentra en el directorio "+this.raiz.identificador;
+				}
+			}else{
+				console.log("ruta absoluta");
+				var objeto = this.raiz.getHijo(parametro[0]);
+
+				if(objeto != null){
+					this.raiz = objeto;
+					document.getElementById("terminalMensaje").innerHTML = "Ahora se encuentra en el directorio "+this.raiz.identificador;
+				}else{
+					document.getElementById("terminalMensaje").innerHTML = "Error: Directorio no existe";
+				}
+
+			}
+		}
 	}
 
-	comando_mkdir(parametros){
+	comando_mkdir(parametro){
 		console.log("usted ah seleccionado el comando mkdir");
+		this.agregarNodo(this.raiz, parametro[0]);
+		document.getElementById("terminalMensaje").innerHTML = "Se creo el directorio "+parametro[0];
 	}
 
-	comando_touch(parametros){
+	comando_touch(parametro){
 		console.log("usted ah seleccionado el comando touch");
 	}
 
-	comando_find(parametros){
+	comando_find(parametro){
 		console.log("usted ah seleccionado el comando find");
 	}
 
-	comando_pwd(parametros){
+	comando_pwd(parametro){
 		console.log("usted ah seleccionado el comando pwd");
 	}
 
-	comando_ls(parametros){
+	comando_ls(parametro){
 		console.log("usted ah seleccionado el comando ls");
 	}
 
